cda34e383c1c7823ce84c0a62e8217d3d9d40483
kjm90110 <kjm90110@naver.com>
2025-07-24 10:09:58 +0900

    개인 작업

--- src/model/eventUtil/StockRetainedEvent.java
+++ src/model/eventUtil/StockRetainedEvent.java
@@ -2,7 +2,12 @@ package model.eventUtil;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 import javax.swing.DefaultListModel;
@@ -10,9 +15,9 @@ import javax.swing.JOptionPane;
 
 import Main.MainModel;
 import Main.MainView;
-import model.StockDividendInfoModel;
 import model.apiUtil.StockDividendInfoAPI;
 import model.vo.StockDividendInfoVO;
+import view.StockRetainedPanel;
 
 // 보유하고 있는 주식에 들어가는 이벤트들을 담는 클래스
 
@@ -23,14 +28,13 @@ public class StockRetainedEvent {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				getUIList(mainModel, mainView);
+//				사용자가 입력 후 버튼을 누르면 검증 후 데이터 보내줌
+				verifiyingInputValue(mainModel, mainView); 
 			}
 		});
 	} // buttonClickEvent
 
 	/*
-	 * 리스트에 회사명, 예상 배당금, 2025년 배당금 지급 현황을 리스트에 담아서 view에 전달해야함
-	 * 
 	 * [기능 별 메소드] 
 	 * 1. 가장 최근 데이터 중에 
 	 * 2. api 리스트 안에서 가장 최근 배당 데이터 리턴 
@@ -38,16 +42,9 @@ public class StockRetainedEvent {
 	 * 4. 보유 주식 수 * 1주당 배당금 곱해서 다음 배당 지급일에 받을 배당금 리턴
 	 */
 	
-	// 사용자가 입력한 회사의
-	// 제일 최근 배당 정보 리턴하는 메소드
-//	   int numberOfretainedStock
 	
-	List<String> UIList = new ArrayList();
-
-//	사용자 입력 값을 검증하고 UI에 표시할 리스트를 리턴
-	public List<String> getUIList(MainModel mainModel, MainView mainView) {
-		
-		UIList.clear(); // List 데이터 초기화
+//	사용자 입력 값을 검증하는 메소드
+	public void verifiyingInputValue(MainModel mainModel, MainView mainView) {
 		
 		String companyNm = mainView.getStockRetainedPanel().getCompanyName();
 		String retainedStock = mainView.getStockRetainedPanel().getNumberOfHoldings();
@@ -62,38 +59,134 @@ public class StockRetainedEvent {
 			JOptionPane.showMessageDialog(mainView, "주식명과 보유 주식 수를 입력해 주세요.", "information",
 					JOptionPane.INFORMATION_MESSAGE);
 		} else {
-			UIList = makeList(mainModel, mainView);
+			makeList(mainModel, mainView); 
 		}
-		return UIList;
 	} // getUIList
 	
-	public List<String> makeList(MainModel mainModel, MainView mainView) {
-		String companyNm = mainView.getStockRetainedPanel().getCompanyName();
-		String retainedStock = mainView.getStockRetainedPanel().getNumberOfHoldings();
+	
+	public void makeList(MainModel mainModel, MainView mainView) {
+		String companyNm = mainView.getStockRetainedPanel().getCompanyName(); // 사용자가 입력한 회사명
+		String retainedStock = mainView.getStockRetainedPanel().getNumberOfHoldings(); // 사용자가 입력한 보유 주식 수
 		int numberOfretainedStock = Integer.parseInt(retainedStock);
 		
-		DefaultListModel<StockDividendInfoVO> list = StockDividendInfoAPI.getApi(companyNm);
 		
-//		int size = list.getSize();
-//		for(int i=size-1; i>0; i--) {
-//			if(list.get(i).getCashDvdnPayDt() != null) {
-//				mainView.getStockRetainedPanel()
-//				.getColumnName()
-//				.addRow(list.get(i).getIsinCdNm(), getExpectedDividend(list.get(i), numberOfretainedStock), dividendPaymentsStatusForThisYear());
-//			}
-//		}
-		return null;
-//		
+		
+		mainView.getStockRetainedPanel()
+		.getColumnName()
+		.addRow(new Object[] {
+				StockRetainedPanel.getCompanyName(), // 회사 이름
+				getExpectedDividend(객체,numberOfretainedStock), // 예상 배당금 계산 결과
+				getDividendPaymentsatus(mainView, mainModel) // 올해 배당금 지급 현황
+			}); 
+		
 	} // makeList
+
 	
 //	예상 배당금 계산 후 String으로 바꾸고 리턴해주는 메소드
 	public String getExpectedDividend(StockDividendInfoVO obj, int numberOfretainedStock) {
 		return Integer.toString(Integer.parseInt(obj.getStckGenrDvdnAmt()) * numberOfretainedStock) + " 원";
 	} // getExpectedDividend
 	
-//	올해 기준 배당 지급 현황 (
 	
-	
+//	배당금 지급 현황을 String으로 리턴하는 메소드
+	public static String getDividendPaymentsatus(MainView mainView, MainModel mainModel) {
+		
+		List<Date> dateList = getStringToDate(getSelectDateForThisYearList(mainView, mainModel));
+		
+		Collections.sort(dateList); // 가장 오래된 날짜부터 정렬(오름차순)
 
+		dateList.stream()
+			// String으로 바꾼 뒤 month만 저장
+			.forEach(date -> date.toString().substring(6, 6)); 
+		
+		int size = dateList.size();
+		String dividendPaymentsatus = ""; // 최종적으로 리턴할 문자열
+		
+		for(int i=0; i<size; i++) {
+			if(!(dateList.indexOf(dateList.get(i)) == size-1)) { // 마지막 요소가 아니라면
+				dividendPaymentsatus = dateList.get(i) + "월, ";
+			} else { // 마지막 요소일 경우
+				dividendPaymentsatus = dateList.get(i) + "월 (총 " + size + "번)";
+			}
+		}
+		return dividendPaymentsatus;
+			
+	} // getDividendPaymentsatus
+	
+	
+	
+//	api 데이터에서 올해의 cashDvdnPayDt만 가져와서 리스트에 저장하는 메소드
+	public static List<String> getSelectDateForThisYearList(MainView mainView, MainModel mainModel) {
+		
+		String companyNm = mainView.getStockRetainedPanel().getCompanyName();
+		String curruntYear = StockRetainedPanel.getYear();
+		
+//		리턴할 리스트 초기화
+		List<String> selectDateList = new ArrayList<String>();
+		
+//		api에서 가져온 회사의 배당 정보
+		DefaultListModel<StockDividendInfoVO> list = StockDividendInfoAPI.getApi(companyNm);
+		
+		int size = list.getSize();
+		
+		for(int i=0; i<size; i++) {
+			String date = list.get(i).getCashDvdnPayDt(); // api 데이터에 있는 날짜
+			String year = date.substring(0, 4);
+			if(!date.isEmpty()&&year.equals(curruntYear)) { // 현재 년도와 같을 경우에만 가져옴
+				selectDateList.add(date);
+			}
+		}
+		return selectDateList; 
+	}
+	
+	
+//	제일 최근 배당 정보 객체를 리턴하는 메소드
+	public static DefaultListModel<StockDividendInfoVO> getRecentDividendInfo(MainView mainView, MainModel mainModel) {
+		
+		String companyNm = mainView.getStockRetainedPanel().getCompanyName();
+		
+		DefaultListModel<StockDividendInfoVO> list = StockDividendInfoAPI.getApi(companyNm);
+		
+//		데이트 포맷
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		int size = list.getSize();
+		
+		try {
+			
+			Date curruntDate = sdf.parse(String.valueOf(LocalDate.now()));
+			
+			
+			
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} // 현재 년월일
+
+		
+		
+	}
+	
+	
+	// String인 날짜 List 요소들을 Date 타입으로 변환 후 List에 담음
+		public static List<Date> getStringToDate(List<String> strList) {
+
+			// 연도월일로 날짜 format
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+
+			
+				// date 담을 ArrayList 생성
+				List<Date> dateList = new ArrayList<Date>();
+				dateList = strList.stream().filter(day -> !day.isEmpty()) // null 제외
+						.map(day -> {
+							try {
+								return sdf.parse(day.toString()); // format으로 date로 변경
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+							return null;
+						}).toList();
+
+				return dateList; // dateList로 반환
+			} // getStringToDate
+	
 	
 } // StockRetainedEvent
